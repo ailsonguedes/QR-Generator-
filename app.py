4e03b27b961b4cc121c0aa085b768a9bf7489a85
ailsonguedes <ailsonsixseven@gmail.com>
2023-09-27 17:27:47 -0300

    New Commit: main aplication comment

--- app.py
+++ app.py
@@ -49,5 +49,6 @@ def docs():
 def gitPage():
     return redirect('https://github.com/ailsonguedes/QR-Generator-/tree/main')
 
+# Call main aplication
 if __name__ == '__main__':
     app.run(debug=True)
